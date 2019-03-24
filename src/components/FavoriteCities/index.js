31ccf4b5f24640c48f694428d04af27f8a269625
Nina Stawski <me@ninastawski.com>
2019-03-24 00:52:00 -0700

    Updated FavoriteCities accorting to .eslintrc

--- src/components/FavoriteCities/index.js
+++ src/components/FavoriteCities/index.js
@@ -1,55 +1,71 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import City from './City';
 import text from '../../text.json';
 import './index.scss';
 
 class FavoriteCities extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            unit: "Imperial"
-        }
-    }
-    
-    unitSwitch = {
-        "Imperial": "Metric",
-        "Metric": "Imperial"
+  static unitSwitch = {
+    Imperial: 'Metric',
+    Metric: 'Imperial'
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      unit: 'Imperial'
     };
-    
-    render() {
-        return ( 
-            <div className="FavoriteCities">
-                <h1 className="FavoriteCities--header">{text.favoriteCities}
-                {this.props.cities && this.props.cities.length ? (
-                <div 
-                    className="FavoriteCities--unitBlock" 
-                    onClick={() => this.setState({"unit": this.unitSwitch[this.state.unit]})}>Unit:&nbsp;
-                    <div className="FavoriteCities--unit">
-                        {this.state.unit === "Imperial" ? "F" : "C"}
-                    </div>
-                </div>
-                ) : ""}
-                </h1>
-                {this.props.cities && this.props.cities.length ? (
-                    <div>
-                        
-                        <div className="FavoriteCities--list">
-                            {this.props.cities.map((city, key) => (
-                                <City 
-                                    key={key} 
-                                    city={city} 
-                                    unit={this.state.unit} 
-                                    handleDelete={this.props.handleDelete}/>
-                                )
-                            )}
-                        </div>
-                    </div>
-                ) : (
-                    <div className="FavoriteCities--empty">You haven't added a favorite city yet.</div>
-                )}
+  }
+
+  render() {
+    const { cities, handleDelete } = this.props;
+    const { unit } = this.state;
+    return (
+      <div className="FavoriteCities">
+        <h1 className="FavoriteCities--header">
+          {text.favoriteCities}
+          {cities && cities.length ? (
+            <div
+              role="button"
+              tabIndex={0}
+              className="FavoriteCities--unitBlock"
+              onClick={() =>
+                this.setState({
+                  unit: FavoriteCities.unitSwitch[unit]
+                })
+              }
+              onKeyPress={() =>
+                this.setState({
+                  unit: FavoriteCities.unitSwitch[unit]
+                })
+              }
+            >
+              Unit:&nbsp;
+              <div className="FavoriteCities--unit">{unit === 'Imperial' ? 'F' : 'C'}</div>
             </div>
-        );
-    }
+          ) : (
+            ''
+          )}
+        </h1>
+        {cities && cities.length ? (
+          <div>
+            <div className="FavoriteCities--list">
+              {cities.map((city, key) => (
+                <City key={city.key} city={city} unit={unit} handleDelete={handleDelete} />
+              ))}
+            </div>
+          </div>
+        ) : (
+          <div className="FavoriteCities--empty"> You haven&#39;t added a favorite city yet.</div>
+        )}
+      </div>
+    );
+  }
 }
+
+FavoriteCities.propTypes = {
+  cities: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  handleDelete: PropTypes.func.isRequired
+};
 
 export default FavoriteCities;
